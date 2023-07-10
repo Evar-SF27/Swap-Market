0006bcabffc1e3547d4da2a9220c59db8b4d9445
Evar-SF27 <evarfomuso29@gmail.com>
2023-07-10 20:39:35 +0100

    login form

--- market/core/urls.py
+++ market/core/urls.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+from .forms import LoginForm
 from . import views
 
 app_name = 'core'
@@ -7,5 +9,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
     path('signup/', views.signup, name='signup'),
 ]
