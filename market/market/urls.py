74686059db60170d86e933724d3a68e021bd82e3
Evar-SF27 <evarfomuso29@gmail.com>
2023-07-10 18:37:43 +0100

    debugging image urls

--- market/market/urls.py
+++ market/market/urls.py
@@ -2,12 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from core.views import index, contact, about
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('items/', include('items.urls')),
-    path('contact/', contact, name='contact'),
-    path('about/', about, name='about'),
+    path('', include('core.urls')),
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
